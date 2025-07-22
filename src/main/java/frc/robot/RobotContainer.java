2fb39d9b6b7c966793ae1bb4b1f92cd217d770d2
Mai961 <michael96mai@gmail.com>
2025-07-22 11:31:12 +0800

    Update at 'Tue Jul 22 11:31:11 CST 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -127,8 +127,8 @@ public class RobotContainer {
           new RollerIOReal(
               RobotConstants.IntakeConstants.INTAKE_MOTOR_ID,
               RobotConstants.CANIVORE_CAN_BUS_NAME,
-              100,
-              100,
+              80,
+              80,
               RobotConstants.IntakeConstants.IS_INTAKER_INVERT,
               RobotConstants.IntakeConstants.IS_BRAKE),
           new RollerIOReal(
